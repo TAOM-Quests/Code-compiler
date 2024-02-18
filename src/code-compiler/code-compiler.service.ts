28073850c87b132d7693fea3c0f24d709ae95395
sophiakars <sofia.karsakova@gmail.com>
2024-02-18 16:43:16 +0400

    update

--- src/code-compiler/code-compiler.service.ts
+++ src/code-compiler/code-compiler.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { spawn } from 'child_process';
-import * as edge from 'edge-js';
-import { writeFile, mkdir, rm } from 'fs/promises';
+import { writeFile, mkdir, rm, rename } from 'fs/promises';
 import { v4 as uuidv4 } from 'uuid';
 import { join } from 'path';
 
@@ -14,7 +13,7 @@ export class CodeCompilerService {
       case 'python':
         return this.executeVariousLanguages(code, 'python', ['-c', code]);
       case 'csharp':
-        return this.executeCSharp(code);
+        return this.executeCS(code);
       case 'java':
         return this.executeJava(code);
       case 'cpp':
@@ -53,37 +52,6 @@ export class CodeCompilerService {
     });
   }
 
-  private executeCSharp(code: string): Promise<string> {
-    return new Promise<string>((resolve, reject) => {
-      const csharpFunc = edge.func(`
-        async (input) => {
-          var originalConsoleOut = Console.Out;
-
-          using (var stringWriter = new System.IO.StringWriter())
-          {
-              Console.SetOut(stringWriter);
-  
-              ${code}
-  
-              string consoleOutput = stringWriter.ToString();
-  
-              Console.SetOut(originalConsoleOut);
-  
-              return consoleOutput;
-          }
-        }
-      `);
-
-      csharpFunc(null, (error, result) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(result.toString());
-        }
-      });
-    });
-  }
-
   private async executeJava(code: string): Promise<string> {
     try {
       const folderName = uuidv4();
@@ -106,6 +74,75 @@ export class CodeCompilerService {
       const executionResult = await this.executeJavaProgram(
         folderName,
         className,
+      );
+
+      await rm(folderName, { recursive: true });
+
+      return executionResult;
+    } catch (error) {
+      return error.message;
+    }
+  }
+  private async executeCpp(code: string): Promise<string> {
+    try {
+      const folderName = uuidv4();
+
+      await mkdir(folderName);
+
+      const fileName = `${uuidv4()}.cpp`;
+
+      const filePath = join(folderName, fileName);
+      await writeFile(filePath, code);
+
+      const execFileName = 'a.exe';
+
+      const compileResult = await this.compileCode('g++', [
+        filePath,
+        '-o',
+        join(folderName, 'a.exe'),
+      ]);
+
+      if (compileResult.stderr) {
+        return compileResult.stderr;
+      }
+
+      const executionResult = await this.executeProgram(
+        folderName,
+        execFileName,
+      );
+
+      await rm(folderName, { recursive: true });
+
+      return executionResult;
+    } catch (error) {
+      return error.message;
+    }
+  }
+
+  private async executeCS(code: string): Promise<string> {
+    try {
+      const folderName = uuidv4();
+
+      await mkdir(folderName);
+
+      const fileName = `${uuidv4()}.cs`;
+
+      const filePath = join(folderName, fileName);
+      await writeFile(filePath, code);
+
+      const execFileName = `${fileName.substring(0, fileName.length - 3)}.exe`;
+
+      const compileResult = await this.compileCode('csc', [filePath]);
+
+      await rename(execFileName, join(folderName, execFileName));
+
+      if (compileResult.stderr) {
+        return compileResult.stderr;
+      }
+
+      const executionResult = await this.executeProgram(
+        folderName,
+        execFileName,
       );
 
       await rm(folderName, { recursive: true });
@@ -168,54 +205,12 @@ export class CodeCompilerService {
       });
     });
   }
-
-  private extractClassName(code: string): string {
-    const classRegex = /class\s+([A-Za-z_$][A-Za-z\d_$]*)/;
-    const match = code.match(classRegex);
-    return match ? match[1] : 'Main';
-  }
-  private async executeCpp(code: string): Promise<string> {
-    try {
-      const folderName = uuidv4();
-
-      await mkdir(folderName);
-
-      const fileName = `${uuidv4()}.cpp`;
-
-      const filePath = join(folderName, fileName);
-      await writeFile(filePath, code);
-
-      const className = 'a.exe';
-
-      const compileResult = await this.compileCode('g++', [
-        filePath,
-        '-o',
-        join(folderName, 'a.exe'),
-      ]);
-
-      if (compileResult.stderr) {
-        return compileResult.stderr;
-      }
-
-      const executionResult = await this.executeCppProgram(
-        folderName,
-        className,
-      );
-
-      await rm(folderName, { recursive: true });
-
-      return executionResult;
-    } catch (error) {
-      return error.message;
-    }
-  }
-
-  private executeCppProgram(
+  private executeProgram(
     folderName: string,
-    className: string,
+    execFileName: string,
   ): Promise<string> {
     return new Promise<string>((resolve, reject) => {
-      const childProcess = spawn(`./${folderName}/${className}`);
+      const childProcess = spawn(`./${folderName}/${execFileName}`);
 
       let output = '';
       let error = '';
@@ -240,5 +235,11 @@ export class CodeCompilerService {
         }
       });
     });
+  }
+
+  private extractClassName(code: string): string {
+    const classRegex = /class\s+([A-Za-z_$][A-Za-z\d_$]*)/;
+    const match = code.match(classRegex);
+    return match ? match[1] : 'Main';
   }
 }
